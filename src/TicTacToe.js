61b2e09e8f2a6c33e2b94e9fef45de7123394e94
astutz96 <astutz96.development@protonmail.com>
2021-04-28 13:27:41 -0400

    generating tiles dynamically from the boardstatus data

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -2,32 +2,15 @@ import React from 'react';
 import Tile from './Components/Tile/tile'
 
 function TicTacToeGame() {
-    const tileSymbol = {
-        X: 'X',
-        Y: 'Y', 
-        Tie: ''
-    }
+    const BoardStatus = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
 
     return (
         <div>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            <Tile tileSymbol='X'/>
-            {/* <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button> */}
+            {
+                BoardStatus.map(symbol => (
+                    <Tile tileSymbol={symbol}/>   
+                ))
+            }
         </div>
     )
 }
