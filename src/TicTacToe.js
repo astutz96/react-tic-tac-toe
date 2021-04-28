23376c36d0720c934c823860ab2221c068b8b7de
astutz96 <astutz96.development@protonmail.com>
2021-04-28 13:15:55 -0400

    adding tiles to game

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Tile from './Components/Tile/tile'
 
-function TicTacToeGame() { 
+function TicTacToeGame() {
+    const tileSymbol = {
+        X: 'X',
+        Y: 'Y', 
+        Tie: ''
+    }
+
     return (
-        <h1>TicTacToe Game</h1>
+        <div>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            <Tile tileSymbol='X'/>
+            {/* <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button> */}
+        </div>
     )
 }
 
